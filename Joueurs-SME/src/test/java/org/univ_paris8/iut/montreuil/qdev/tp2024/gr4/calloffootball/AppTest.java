a4371d10bb7bcf21e7e33fb47b490695906c3a1b
amineakhrib <amine.akhrib@europcar.com>
2024-05-07 11:32:18 +0200

    premier push du goat

---
+++ Joueurs-SME/src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr4/calloffootball/AppTest.java
@@ -0,0 +1,20 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr4.calloffootball;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
